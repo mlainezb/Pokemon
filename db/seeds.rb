f0967113ca994dcaf63e5a557380d319bd1dd162
manuel <mlainezb@gmail.com>
2016-11-15 20:13:07 -0300

    Modelo scaffold Pokemon y Seed OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pokemons = Pokemon.create ([
+	{ 
+		name: 'Charmander',  
+		description: 'Fire' 
+	},
+	{ 	name: 'Metapod',
+		description: 'Fire' 
+	},
+	{
+		name: 'Squirtle',
+		description: 'Bug'
+	},
+	{
+		name: 'Arbok',
+		description: 'Poison'
+	},
+	{
+		name: 'Primeape',
+		description: 'Fighting'
+	}
+	])
